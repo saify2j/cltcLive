c7c0af6eb5594fed7226755d58322fa8a662e38b
Saif Kamal Chowdhury <saifx2y@gmail.com>
2020-04-01 18:23:22 +0600

    Updates

---
+++ precache-manifest.22029b6eabca65d855f899e0445ef214.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45019f5c3c3be720d298b6ea8c6bd563",
+    "url": "/cltcLive/index.html"
+  },
+  {
+    "revision": "0c528f1370f158bd4c0b",
+    "url": "/cltcLive/static/css/main.f106a2ec.chunk.css"
+  },
+  {
+    "revision": "1e4690d31402705fbf9e",
+    "url": "/cltcLive/static/js/2.61f59960.chunk.js"
+  },
+  {
+    "revision": "0c528f1370f158bd4c0b",
+    "url": "/cltcLive/static/js/main.4aebbf3f.chunk.js"
+  },
+  {
+    "revision": "da21b5ecf822d4e7f572",
+    "url": "/cltcLive/static/js/runtime~main.88fa483f.js"
+  },
+  {
+    "revision": "db7ddd5a4c6bcd2cbe349110b51daaeb",
+    "url": "/cltcLive/static/media/avatar.db7ddd5a.png"
+  },
+  {
+    "revision": "3165c186e5879ca5ee9dfd119953d245",
+    "url": "/cltcLive/static/media/icon-audience-inactived.3165c186.png"
+  },
+  {
+    "revision": "2620e52585f92615e6ab5ee899c119f6",
+    "url": "/cltcLive/static/media/icon-audience.2620e525.png"
+  },
+  {
+    "revision": "25174b6ec7aee7a101e68cdf6d7143a8",
+    "url": "/cltcLive/static/media/icon-host-inactived.25174b6e.png"
+  },
+  {
+    "revision": "6baf9f5c788826fe3e42804dec3b89ab",
+    "url": "/cltcLive/static/media/icon-host.6baf9f5c.png"
+  },
+  {
+    "revision": "41c0dce84904b9e62385f51a888a73c1",
+    "url": "/cltcLive/static/media/logo-open-live-gray.41c0dce8.png"
+  },
+  {
+    "revision": "1cccf552e25f6614e1b734b29eb4b94d",
+    "url": "/cltcLive/static/media/logo-open-live.1cccf552.png"
+  }
+]);
